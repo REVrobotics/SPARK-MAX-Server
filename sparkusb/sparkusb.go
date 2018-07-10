6c1b6954ad47321818eec510f929dd4aa71b661b
Will Toth <wtoth1631@gmail.com>
2018-07-09 20:31:30 -0500

    Added connect and disconnect methods

--- sparkusb/sparkusb.go
+++ sparkusb/sparkusb.go
@@ -3,8 +3,24 @@ package sparkusb
 import (
 	"log"
 
+	serial "go.bug.st/serial.v1"
 	enumerator "go.bug.st/serial.v1/enumerator"
 )
+
+const (
+	spark2PID   = "5740"
+	sparkTNGPID = "5740"
+	sparkVID    = "0483"
+)
+
+var localPort serial.Port
+
+func isSparkPID(pid string) (isSpark bool) {
+	if spark2PID == pid || sparkTNGPID == pid {
+		isSpark = true
+	}
+	return
+}
 
 /*ListDevices (all) set all to true to include connected CAN devices
 * Note: 'All' parameter is not yet implemented
@@ -17,9 +33,38 @@ func ListDevices(all bool) []*enumerator.PortDetails {
 
 	tmp := ports[:0]
 	for _, port := range ports {
-		if port.PID == "5740" && port.VID == "0483" {
+		if isSparkPID(port.PID) && port.VID == sparkVID {
 			tmp = append(tmp, port)
 		}
 	}
 	return tmp
+}
+
+func connect(com string) {
+	mode := &serial.Mode{
+		BaudRate: 9600,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+	port, err := serial.Open(com, mode)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	localPort = port
+}
+
+func disconnect() {
+	if localPort == nil {
+		return
+	}
+
+	err := localPort.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	localPort = nil
 }
