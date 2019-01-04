25d511246815402fc77fc9f0ffe6a35c12cf8e5d
Will Toth <toth@revrobotics.com>
2019-01-04 17:14:41 -0600

    Removed old client from repo

--- sparkusb-client/gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulp = require("gulp");
-
-gulp.task("post-build", () => {
-  gulp.src("sparkusb/**/*").pipe(gulp.dest("build/sparkusb"));
-  gulp.src("main/**/*").pipe(gulp.dest("build/main"));
-  gulp.src("icon.png").pipe(gulp.dest("build/resources"));
-});
